d1d5eacdede8ce760fc3d6d7daa5a82604acd881
Paris <bemorales@scl.edu.gt>
2025-05-17 12:44:54 -0600

    diseño visual terminado

--- components/pantalla4/pantalla4.js
+++ components/pantalla4/pantalla4.js
@@ -1,6 +1,22 @@
 function pantalla4() {
-  let pantalla4 = document.createElement("div");
-  pantalla4.className = "pantalla4";
+  // Crear contenedor flip (igual que en pantalla3)
+  let flipContainer = document.createElement("div");
+  flipContainer.className = "flip-container";
+
+  let flipper = document.createElement("div");
+  flipper.className = "flipper";
+
+  // --- PANTALLA FRONTAL ---
+  let front = document.createElement("div");
+  front.className = "front pantalla4";
+
+  // Elementos existentes de pantalla4
+  let h1_p4 = document.createElement("h1");
+  h1_p4.textContent = "te amo muchote";
+
+  let img_p4 = document.createElement("div");
+  img_p4.innerHTML = `<img src="./assets/love3.png" alt="">`;
+  img_p4.className = "img_p4";
 
   let yellowShape4 = document.createElement("div");
   yellowShape4.className = "shape yellow-shape4";
@@ -8,9 +24,48 @@ function pantalla4() {
   let redShape4 = document.createElement("div");
   redShape4.className = "shape red-shape4";
 
-  pantalla4.append(yellowShape4, redShape4);
+  // Botón girar (nuevo, como en pantalla3)
+  let btn_girar = document.createElement("button");
+  btn_girar.className = "btn_girar4";
+  btn_girar.textContent = "click here";
 
-  return pantalla4;
+  let nota4 = document.createElement("div");
+  nota4.className = "nota4";
+  nota4.textContent = "Abre tu regalo especial";
+
+  // Ensamblar frontal
+  front.append(yellowShape4, redShape4, img_p4, h1_p4, btn_girar, nota4);
+
+  // --- PANTALLA TRASERA ---
+  let back = document.createElement("div");
+  back.className = "back pantalla_trasera4";
+
+  // Botón regresar
+  let btn_regresar = document.createElement("button");
+  btn_regresar.className = "btn_regresar4";
+  btn_regresar.textContent = "<";
+
+  // Contenido trasero (personalizado para pantalla4)
+  let mensajeTrasero = document.createElement("h1");
+  mensajeTrasero.textContent = "Eres lo mejor que me ha pasado";
+
+  // Ensamblar trasera
+  back.append(btn_regresar, mensajeTrasero);
+
+  // Unir todo
+  flipper.append(front, back);
+  flipContainer.appendChild(flipper);
+
+  // Event listeners (igual que en pantalla3)
+  btn_girar.addEventListener("click", () => {
+    flipContainer.classList.add("flipped");
+  });
+
+  btn_regresar.addEventListener("click", () => {
+    flipContainer.classList.remove("flipped");
+  });
+
+  return flipContainer;
 }
 
 export { pantalla4 };
--- components/pantalla5/pantalla5.js
+++ components/pantalla5/pantalla5.js
@@ -1,6 +1,22 @@
 function pantalla5() {
-  let pantalla5 = document.createElement("div");
-  pantalla5.className = "pantalla5";
+  // Crear contenedor flip (igual que en pantalla4)
+  let flipContainer = document.createElement("div");
+  flipContainer.className = "flip-container";
+
+  let flipper = document.createElement("div");
+  flipper.className = "flipper";
+
+  // --- PANTALLA FRONTAL ---
+  let front = document.createElement("div");
+  front.className = "front pantalla5";
+
+  // Elementos existentes de pantalla5
+  let h1_p5 = document.createElement("h1");
+  h1_p5.textContent = "figaro te quiere mucho";
+
+  let img_p5 = document.createElement("div");
+  img_p5.innerHTML = `<img src="./assets/figaro.png" alt="">`;
+  img_p5.className = "img_p5";
 
   let yellowShape5 = document.createElement("div");
   yellowShape5.className = "shape yellow-shape5";
@@ -8,9 +24,48 @@ function pantalla5() {
   let redShape5 = document.createElement("div");
   redShape5.className = "shape red-shape5";
 
-  pantalla5.append(yellowShape5, redShape5);
+  // Botón girar (como en pantalla4)
+  let btn_girar = document.createElement("button");
+  btn_girar.className = "btn_girar5";
+  btn_girar.textContent = "click here";
 
-  return pantalla5;
+  let nota5 = document.createElement("div");
+  nota5.className = "nota5";
+  nota5.textContent = "Abre tu regalo especial";
+
+  // Ensamblar frontal
+  front.append(yellowShape5, redShape5, h1_p5, img_p5, btn_girar, nota5);
+
+  // --- PANTALLA TRASERA ---
+  let back = document.createElement("div");
+  back.className = "back pantalla_trasera5";
+
+  // Botón regresar
+  let btn_regresar = document.createElement("button");
+  btn_regresar.className = "btn_regresar5";
+  btn_regresar.textContent = "<";
+
+  // Contenido trasero (personalizado para pantalla5)
+  let mensajeTrasero = document.createElement("h1");
+  mensajeTrasero.textContent = "Eres su humano favorito";
+
+  // Ensamblar trasera
+  back.append(btn_regresar, mensajeTrasero);
+
+  // Unir todo
+  flipper.append(front, back);
+  flipContainer.appendChild(flipper);
+
+  // Event listeners (igual que en pantalla4)
+  btn_girar.addEventListener("click", () => {
+    flipContainer.classList.add("flipped");
+  });
+
+  btn_regresar.addEventListener("click", () => {
+    flipContainer.classList.remove("flipped");
+  });
+
+  return flipContainer;
 }
 
 export { pantalla5 };
